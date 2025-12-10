a2e645251f64286faed3891883a2e716d36c91d6
btbuilds <bthompson8807@gmail.com>
2025-12-09 20:27:12 -0500

    Initial implementation of discord bot.

---
+++ src/discord-bot/main.py
@@ -0,0 +1,35 @@
+import discord
+from discord.ext import commands
+from discord import app_commands
+from bot_token import BOT_TOKEN
+from server_id import SERVER_ID
+
+GUILD_ID = discord.Object(id=SERVER_ID)
+
+class Client(commands.Bot):
+    async def on_ready(self):
+        print(f'Logged on as {self.user}')
+
+        try:
+            synced = await self.tree.sync(guild=GUILD_ID)
+            print(f'Synced {len(synced)} commands to guild {GUILD_ID}')
+        except Exception as e:
+            print(f'Error syncing commands: {e}')
+
+intents = discord.Intents.default()
+intents.members = True
+intents.message_content = True
+
+
+
+client = Client(command_prefix="!", intents=intents)
+
+@client.tree.command(name="hello", description="Just says hello", guild=GUILD_ID)
+async def say_hello(interaction: discord.Interaction):
+    await interaction.response.send_message("Hello")
+
+@client.tree.command(name="printer", description="Prints arguments", guild=GUILD_ID)
+async def printer(interaction: discord.Interaction, printer: str):
+    await interaction.response.send_message(printer)
+
+client.run(BOT_TOKEN)
